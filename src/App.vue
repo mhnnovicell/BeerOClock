25271560e994d94dcba6468a6e4d0ef7c7cbf464
mninozebra <mni@nozebra.dk>
2018-10-30 10:05:50 +0100

    Added some new styles and responsive design

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <main>
-      <h1 class="main-headline">Beer'O'Clock</h1>
+      <img src="./assets/logo.svg" class="bodylogo" alt="bodylogo">
       <router-view></router-view>
     </main>
     <footer>
-      <img src="../static/img/nzlogo.png" alt="nozebralogo" class="footerlogo">
+      <img src="./assets/nzlogo.png" alt="nozebralogo" class="footerlogo">
     </footer>
   </div>
 </template>
@@ -17,137 +17,112 @@
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css?family=Lato|Montserrat|Pacifico');
+html {
+  font-size: 1rem;
+}
 
+@media screen and (min-width: 319px) {
   html {
-    font-size: 1rem;
-    color: white;
+    font-size: 1.2rem;
   }
+}
 
-
-  body {
-    margin: 0;
-    animation: colorchange 30s infinte;
-    /* animation-name followed by duration in seconds*/
-    /* you could also use milliseconds (ms) or something like 2.5s */
-    -webkit-animation: colorchange 30s infinite;
-    /* Chrome and Safari */
+@media screen and (min-width: 767px) {
+  html {
+    font-size: 1.4rem;
   }
+}
 
-  @keyframes colorchange {
-    0% {
-      background: #13547a;
-    }
-    25% {
-      background: #80d0c7;
-    }
-    50% {
-      background: #f6d365;
-    }
-    75% {
-      background: #fda085;
-    }
-    100% {
-      background: #ff5858;
-    }
+@media screen and (min-width: 1439px) {
+  html {
+    font-size: 1.6rem;
   }
+}
+body {
+  margin: 0;
+  animation: colorchange 30s infinte;
+  /* animation-name followed by duration in seconds*/
+  /* you could also use milliseconds (ms) or something like 2.5s */
+  -webkit-animation: colorchange 30s infinite;
+  /* Chrome and Safari */
+}
 
-  @-webkit-keyframes colorchange
-  /* Safari and Chrome - necessary duplicate */
-
-  {
-    0% {
-      background: #13547a;
-    }
-    25% {
-      background: #80d0c7;
-    }
-    50% {
-      background: #f6d365;
-    }
-    75% {
-      background: #fda085;
-    }
-    100% {
-      background: #ff5858;
-    }
+@keyframes colorchange {
+  0% {
+    background: #13547a;
   }
-
-  #app {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+  25% {
+    background: #80d0c7;
   }
-
-  main {
-    text-align: center;
-    margin-top: 40px;
+  50% {
+    background: #f6d365;
   }
-
-
-  .footerlogo {
-    position: absolute;
-    bottom: 10px;
-    right: 10px;
-    width: 5vw;
+  75% {
+    background: #fda085;
   }
-
-  .bodylogo {
-    width: 50vw;
+  100% {
+    background: #ff5858;
   }
+}
 
-  .time-unite-title {
-    font-family: 'Montserrat', sans-serif;
-    font-weight: 300;
-    line-height: normal;
-    text-align: center;
-    letter-spacing: 0.02em;
-    color: rgba(255, 255, 255, 0.5);
-    font-size: 1.5rem;
+@-webkit-keyframes colorchange
+  /* Safari and Chrome - necessary duplicate */ {
+  0% {
+    background: #13547a;
   }
-
-  .time-unite-value {
-    font-family: 'Lato', sans-serif;
-    font-weight: bold;
-    line-height: normal;
-    text-align: center;
-    font-size: 2rem;
+  25% {
+    background: #80d0c7;
   }
-
-  .main-headline {
-    font-family: 'Pacifico', cursive;
-    font-size: 4rem;
+  50% {
+    background: #f6d365;
   }
-
-  .countdown {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
+  75% {
+    background: #fda085;
   }
-
-    @media screen and (min-width: 319px) and (max-width: 450px) {
-    html {
-
-      font-size: 1.2rem;
-    }
-
-     .footerlogo {
-
-    width: 25vw;
+  100% {
+    background: #ff5858;
   }
-  }
+}
 
-  @media screen and (min-width: 767px) and (max-width: 1025px) {
-    html {
-      font-size: 1.4rem;
-    }
-  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
 
-  @media screen and (min-width: 1026px) {
-    html {
-      font-size: 1.6rem;
-    }
-  }
+main {
+  text-align: center;
+  margin-top: 40px;
+}
 
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #35495e;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: 0.02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+
+.footerlogo {
+  position: relative;
+  bottom: 0;
+  left: 0;
+}
+
+.bodylogo {
+  width: 50vw;
+}
 </style>
-
 
--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -1,88 +1,75 @@
 <template>
-  <div class="countdown">
-    <div class="time-unite created" v-show="years">
-      <div class="time-unite-value">{{ years }}</div>
-      <div class="time-unite-title">years</div>
-    </div>
-    <div class="time-unite created" v-show="months">
-      <div class="time-unite-value">{{ months }}</div>
-      <div class="time-unite-title">months</div>
-
-    </div>
-    <div class="time-unite created" v-show="days">
-      <div class="time-unite-value">{{ days }}</div>
-      <div class="time-unite-title">days</div>
-
-    </div>
-    <div class="time-unite created">
-      <div class="time-unite-value">{{ hours }}</div>
-      <div class="time-unite-title">hours</div>
-
-    </div>
-    <div class="time-unite created">
-      <div class="time-unite-value">{{ minutes }}</div>
-      <div class="time-unite-title">minutes</div>
-
-    </div>
-    <div class="time-unite created">
-      <div class="time-unite-value">{{ seconds }}</div>
-      <div class="time-unite-title">seconds</div>
-
-
-    </div>
-  </div>
+	<div class="countdown">
+	    <div class="time-unite created" v-show="years">
+	        <div class="time-unite-title">Y</div>
+	        <div class="time-unite-value">{{ years }}</div>
+	    </div>
+	    <div class="time-unite created" v-show="months">
+	        <div class="time-unite-title">Måneder</div>
+	        <div class="time-unite-value">{{ months }}</div>
+	    </div>
+	    <div class="time-unite created" v-show="days">
+	        <div class="time-unite-title">Dage</div>
+	        <div class="time-unite-value">{{ days }}</div>
+	    </div>
+	    <div class="time-unite created">
+	        <div class="time-unite-title">Timer</div>
+	        <div class="time-unite-value">{{ hours }}</div>
+	    </div>
+	    <div class="time-unite created">
+	        <div class="time-unite-title">Minutter</div>
+	        <div class="time-unite-value">{{ minutes }}</div>
+	    </div>
+	    <div class="time-unite created">
+	        <div class="time-unite-title">Sekunder</div>
+	        <div class="time-unite-value">{{ seconds }}</div>
+	    </div>
+	</div>
 </template>
 
 <script>
-  import moment from "moment";
-
-
-
-  export default {
-    data() {
-      return {
-        actualTime: moment().format("X"),
-        years: 0,
-        months: 0,
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-        beeroclock: "BeerOClock!"
-      };
-    },
-    methods: {
-      addOneSecondToActualTimeEverySecond() {
-        var component = this;
-        component.actualTime = moment().format("X");
-        setTimeout(function() {
-          component.addOneSecondToActualTimeEverySecond();
-        }, 1000);
-      },
-      getDiffInSeconds() {
-        return moment("2018-10-26 15:00:00").format("X") - this.actualTime;
-      },
-      compute() {
-        var duration = moment.duration(this.getDiffInSeconds(), "seconds");
-        this.years = duration.years() > 0 ? duration.years() : 0;
-        this.months = duration.months() > 0 ? duration.months() : 0;
-        this.days = duration.days() > 0 ? duration.days() : 0;
-        this.hours = duration.hours() > 0 ? duration.hours() : 0;
-        this.minutes = duration.minutes() > 0 ? duration.minutes() : 0;
-        this.seconds = duration.seconds() > 0 ? duration.seconds() : 0;
-      }
-    },
-    created() {
-      this.compute();
-      this.addOneSecondToActualTimeEverySecond();
-      this.beeroclock;
-    },
-    watch: {
-      actualTime(val, oldVal) {
-        this.compute();
-      }
-    }
-  };
+	import moment from 'moment'
+	export default {
+		data () {
+			return {
+				actualTime: moment().format('X'),
+				years: 0,
+				months: 0,
+				days: 0,
+				hours: 0,
+				minutes: 0,
+				seconds: 0
+			}
+		},
+		methods: {
+		  addOneSecondToActualTimeEverySecond () {
+		      var component = this
+		      component.actualTime = moment().format('X')
+		      setTimeout(function(){
+		          component.addOneSecondToActualTimeEverySecond()
+		      }, 1000);
+		  },
+		  getDiffInSeconds () {
+		    return moment("2018-11-02 15:00:00").format('X') - this.actualTime
+		  },
+		  compute () {
+		    var duration = moment.duration(this.getDiffInSeconds(), "seconds")
+		    this.years = duration.years() > 0 ? duration.years() : 0
+		    this.months = duration.months() > 0 ? duration.months() : 0
+		    this.days = duration.days() > 0 ? duration.days() : 0
+		    this.hours = duration.hours() > 0 ? duration.hours() : 0
+		    this.minutes = duration.minutes() > 0 ? duration.minutes() : 0
+		    this.seconds = duration.seconds() > 0 ? duration.seconds() : 0
+		  }
+		},
+		created () {
+		  this.compute()
+		  this.addOneSecondToActualTimeEverySecond()
+		},
+		watch: {
+		    actualTime (val,oldVal) {
+		        this.compute()
+		    }
+		}
+	}
 </script>
-
-
